8fc286a12b3584c3aab343a1d796b26d7a9a5684
kadirhas <31936408+kadirhas@users.noreply.github.com>
2018-05-27 19:58:32 +0300

    increased the number of epochs

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def run():
         logits, train_op, cross_entropy_loss = optimize(layer_output, correct_label, learning_rate, num_classes)
 
         # TODO: Train NN using the train_nn function
-        epochs = 6
+        epochs = 50
         batch_size = 5
         saver = tf.train.Saver()
         train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label, keep_prob, learning_rate)
